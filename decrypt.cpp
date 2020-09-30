391e061b65a1bed0e058f4d38489c9dfb864c1ca
Amogh_koushik <amoghnkoushik@gmail.com>
2020-10-01 00:17:35 +0530

    Adding Decrypt cpp files
    Using this you can decrypt your messages.
    Hacktober fest request.

---
+++ decrypt.cpp
@@ -0,0 +1,33 @@
+#include <iostream> 
+#include <vector>
+#include <string>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+int main()
+{
+    cout << "Enter your code to decode it" << endl;
+    string user_typo;
+    getline(cin , user_typo);
+    
+    string alphabets {" 'abc1de@fg3hi=jkl2>;m{n9o!+p|8\"q%r<[s6t*u-vw&x]yz,5A}_:B4#C.D(E7FGH$I)JK/LMN^OP0QRSTUVWXYZ"};
+    string encrypted_alphabets {"qwe+r0t\"y3ui{]^op8a}@s=d2*fgh(j7,&5k[#9:lz!x_c4v.bn|;/m$LKJH'GFDSA%1POI6U-Y<TRE>WQMNBVCXZ) "};
+    string final_encryption {};
+    
+    cout << "Decrypting message................." << endl;
+    
+    for (auto i: user_typo) {
+        size_t position = encrypted_alphabets.find(i);
+        if ( position == string::npos ){
+            final_encryption += i;
+        }
+        else {
+           char a { alphabets.at(position)};
+           final_encryption += a;
+        }
+    }
+    cout << final_encryption << endl;
+    return 0;
+}
